fb3c96eafb6a187a7bdc523f49763f5e8a9ccbd9
Zino Hofmann <zino@hofmann.amsterdam>
2016-12-09 10:45:59 +0100

    Added new training images and trained the classifier

--- streamImage.m
+++ streamImage.m
@@ -3,18 +3,11 @@ cam = webcam(1);
 preview(cam);
 pause(5);
 
-snap = snapshot(cam);
-pause(1);
-fname = ('stream.bmp');
-imwrite(snap, fullfile('./streamData', fname));
-imshow(snap);
+while (true)
+    snap = snapshot(cam);
+    pause(1);
+    imwrite(snap, fullfile('./streamData', 'stream.bmp'));
+    [labelIndex, score] = predict(classifier, imread(fullfile('./streamData', 'stream.bmp')));
+end 
 
-
-path = fullfile('./streamData', 'stream.bmp');
-img = imread(path);
-[labelIdx, scores] = predict(classifier, img);
-
-% Display the string label
-classifier.Labels(labelIdx)
-
-clear cam
+clear cam;
